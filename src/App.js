8ba366ec4bf670d2c3b13091bd621285ab970dcf
fathah1 <67840377+fathah1@users.noreply.github.com>
2020-09-09 22:44:57 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+//RANDOM QUOTE MACHINE PROJECT
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 class="main-header">Random Quote Generator.</h1>
+        <h2 class="tag-line">100's of quotes for you to learn from</h2>
       </header>
+
+        <div id="quote-box">
+            <h2 id="text">"Add quotes from API"</h2>
+            <h3 id="author">:- author's name</h3> 
+            <div class="buttons"> 
+              <a id="tweet-quote" class="fa fa-twitter" target="_blank" href="twitter.com/intent/tweet"></a>
+              <button id="new-quote">New Quote</button>
+            </div>
+        </div>
+      
     </div>
   );
 }
